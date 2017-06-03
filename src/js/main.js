1f00790ceaef55872929f6271e593b0940ac672a
Sergei <zubkov@smoothie.digital>
2017-06-03 23:06:25 +0300

    1) Настройка gulp, git - первый коммит

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sergei on 03.06.17.
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+//= partials/app.js
+
+
+//asdasd
---
+++ src/js/partials/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by sergei on 03.06.17.
+ */
+console.log(132)
